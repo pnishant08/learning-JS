7a7bb797869baaeff2e0f41d046cdedf1d0cb42e
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-02-27 14:15:03 +0000

    datatypes

---
+++ 01basic/02datatypes.js
@@ -0,0 +1,28 @@
+"user strict";//treat all js code as newer version 
+//alert(3+3)//we are using nodejs , not browser
+
+
+console.log(3+3); //use code reliability 
+
+console .log("nishant")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint 
+// string =>""
+// null  => standalone value
+//undefined =>value not assigned
+// symbol
+
+//object
+
+console.log(typeof "hitesh")//it show string
+
+console.log(typeof null)//it show object 
+
+
+
