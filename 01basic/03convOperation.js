ad3f0f7ed29c88415404d6990cbad436320cf625
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-02-27 15:52:25 +0000

    conversion of data

---
+++ 01basic/03convOperation.js
@@ -0,0 +1,37 @@
+let score ="33"
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let score1="33an"//show number hi krega but number nahi hai to number bnate hue dhyan rakhna isse
+let valueInNumber1= Number(score1)
+//console.log(typeof valueInNumber1)
+//console.log(valueInNumber1)
+
+
+
+//agr score ki value null krde to print karne pe 0 print hoga
+//agr undefined likh de to Nan aayega
+//if bool true then value =1 or false then value =0
+//if score =hitesh then Nan==not a number
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1=>true ; 0=> false
+//"" =>false
+//"hitesh" => true
+
+
+let somenumber=33;
+let stringNumber=String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
