595d5de4436c9f3c5c72444b4d883ce30886ca53
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-03-16 10:25:26 +0000

    array

--- 02basic/01_array.js
+++ 02basic/01_array.js
@@ -1,1 +1,38 @@
 //array
+const mtArr=[9,1,2,3,4,5,6]
+//console.log(mtArr[0])
+
+const myHeros = ["shaktiman","naagraaj"];
+
+const myArr2=new Array(1,2,3,4);//method of initialise array
+//console.log(myArr2.length);
+
+//methods of array
+
+myArr2.push(9);//value insert kara dega 
+// console.log(myArr2);
+
+myArr2.pop();//last value of array ko pop kara dega
+// console.log(myArr2);
+
+myArr2.unshift(9);//isse jo push hoga front me jayega aur iski wajah se sare element idhar udhar honge
+// console.log(myArr2);
+
+myArr2.shift()//isse kya hoga jo sabse aage wala hoga wo pop ho jayega 
+
+//console.log(myArr2.includes(9));
+
+//indexOf(3)->give index of 3;
+
+const newarr =myArr2.join();
+// console.log(myArr2)
+// console.log(newarr)
+// console.log(typeof myArr2)
+// console.log(typeof newarr)
+
+//slice , splice 
+//slice orignal array ko manipulate nhi karta aur last range nhi print karta 
+
+//splice orignal array ko manipulate karta hai aur last range tak print karta hai
+
+
