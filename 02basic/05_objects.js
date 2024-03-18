7b7321bbbe918d5d9e8e4fdf3d72289417fdac0b
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-03-18 17:35:24 +0000

    json talk

--- 02basic/05_objects.js
+++ 02basic/05_objects.js
@@ -1,2 +1,46 @@
 //destructuring hoti hai sabki
 
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+
+}
+
+//course.courseInsturctor
+
+const{courseInstructor:ins}=course;
+//console.log(courseInstructor)//ye bhi tareeka hai
+//console.log(ins)//ye bhi hai aur isme syntax kam hoga
+
+
+
+
+//ye react ka hai
+
+// const navbar=({company})=>{
+
+// }
+// console.log(navbar(company="hitesh"))
+
+
+ //pehle xml me aata hai baad me json me aane laga
+
+//  {
+//     "name" : "nishant",
+//     "coursename":"js in hindi ",
+//    " price ": "free"
+//  }//api ka hai ye
+
+
+//kabhi kabhi array ke form me ataa hai 
+
+// {
+//     {},
+//     {},
+//     {}
+// }
+
+//js formator se api padha jata hai
+
+ 
