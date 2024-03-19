1ca1909d70b45e4f2191cbcb2adcede97ef09015
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-03-19 19:40:09 +0000

    scope and muni hosting

--- 03basic/02_scope.js
+++ 03basic/02_scope.js
@@ -1,4 +1,46 @@
 //let aur const block scope hai aur 
 //var har jagah change ho jata hai
 
-//similarly har jagah hota hai
+// //similarly har jagah hota hai
+
+// var c = 3330
+
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30
+// }
+
+
+// function one(){
+
+//     const username ="nishant"
+//     function two(){
+//         const website="utube"
+//         console.log(username)
+//     }
+// //console.log(two)
+//     two()
+// }
+// one();
+//child parent ke variable ko access kr skta hai vice versa not possible
+
+//same if ke sath bhi hota hai
+
+///**************************************8 */
+
+
+//agr yha addone call kru to chal jayega
+function addone(num){
+    return num+1
+}
+addone(5)
+
+
+
+//agr yha main addtwo likhu to array hoga
+
+const addtwo=function(num){
+    return num+2
+}
+addtwo(3)
