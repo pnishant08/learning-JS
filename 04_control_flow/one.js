311e85279e8332f64fe8ec363561843ded85a180
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-04-06 21:57:08 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+//if else statement
+
+//comparisons conditions   <,>,<=,>=,==,!=,=== it check its type also in triple equal
+
+//!== ye bhi type ko check karta hai
+
+
+// const score =200
+
+// if(score>100){
+//     const power ="fly"
+//     console.log(`user power : ${power}`)
+// }
+
+// const balance =1000  aise nhi likhna hai company
+// if(balance>500)console.log("test"),console.log("test ")
+
+
+// real life jaise login ho mail dalo aur debit carrd ho tab buy  kar sakte hai
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,36 @@
+// const userEmail="nishant.ai"
+
+// if(userEmail){
+//     console.log("got email")
+// }
+// else{
+//     console.log("dont have mail")
+// }
+
+//falsy value =>   false,0,-0,BigInt 0n ,"",null,undefined, NaN
+
+//truthy value =>  "0",'false'," ",[],{},function(){}
+
+//agr check krna hai to length check kr le 
+//userEmail.length===0
+
+//object ke liye
+//Object.keys(object name).length===0;
+
+
+
+// Nullish Coalescing Operator(??):null undefined
+
+
+let val1;
+// val1=5??10
+// val1=null??10
+
+// val1=undefined??15
+// console.log(val1)
+
+
+//ternary operator
+
+// condition?true:false
+
