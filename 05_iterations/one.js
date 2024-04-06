38397dce11a1c61d73e2066a8378c5774521cb92
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-04-06 23:07:17 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+//for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
+// }
+
+//bahar access nhi hoga scope ki wajah se
+
+// let myArray=["flash","batman","thor"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+    
+// }
+
+//break and continue
