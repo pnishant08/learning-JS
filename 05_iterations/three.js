386ce5124b33383150b881ea2c6405827f64375c
Nishant <105911701+pnishant08@users.noreply.github.com>
2024-04-07 00:05:20 +0000

    loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+//for of 
+
+// ["",""]
+// [{},{},{}]
+
+
+// const arr=[1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+//map
+
+const map=new Map()
+
+map.set('in',"india")
+map.set('usa',"united state of america")
+
+
+console.log(map)
+
+// for (const key of map) {
+//     console.log(key)
+// }
+// for (const [key,value] of map) {
+//     console.log(key,':-',value)
+// }
+
+
+//object ke liye ye use nhi hoga
+
---
+++ 05_iterations/while.js
@@ -0,0 +1,14 @@
+// while (condition) {
+    
+// }
+
+
+// do {
+    
+// } while (condition);
+
+let score =0
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<10);//agr pehle score condition se jyada ho to pehle print hoga fir condition check hoga
